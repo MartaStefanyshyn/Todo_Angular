6b294a03a1415caf53820938404c802c5f524da9
Marta Stefanyshyn <martaolehivna@gmail.com>
2015-12-28 18:03:56 +0200

    Generation of app with gulp-generator

---
+++ ang_gulp/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularTutorial', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
